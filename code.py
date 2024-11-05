4f9981fdaf19a9cd7f23c43dd19e1988d0d9698a
Mehmet Hasat Serinkan <91607536+hasatsrnkn@users.noreply.github.com>
2024-11-05 20:17:59 +0300

    Add or update file in branch

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "21901649"
+    return "123123123"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
